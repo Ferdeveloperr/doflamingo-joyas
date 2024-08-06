cf1253b91871bfde159af851dd5283b42bcc2be0
Ferdeveloperr <fernandojgom1395@gmail.com>
2024-08-06 17:09:48 -0300

    update style navbar in proccess

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,20 @@ export function Header () {
     return (
         <>
 
-    <nav className="bg-blue-500 p-4">
-    <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-xl font-bold">Mi E-commerce</div>
-        <ul className="flex space-x-4">
-            <li><a href="#" className="text-white hover:text-gray-200">Home</a></li>
-            <li><a href="#" className="text-white hover:text-gray-200">Productos</a></li>
-            <li><a href="#" className="text-white hover:text-gray-200">Carrito</a></li>
-            <li><a href="#" className="text-white hover:text-gray-200">Contacto</a></li>
-        </ul>
-    </div>
-    </nav>
+        <nav className="bg-black p-1">
+            <div className="container mx-auto flex justify-between items-center">
+            <div className="flex items-center">
+            <img src="./public/logo-doflamingo.png" alt="Mi E-commerce" className="h-16 w-16" />
+            </div>
+
+                <ul className="flex space-x-4">
+                    <li><a href="#" className="styled-text" data-text="Home">Home</a></li>
+                    <li><a href="#" className="styled-text" data-text="Productos">Productos</a></li>
+                    <li><a href="#" className="styled-text" data-text="Contacto">Contacto</a></li>
+                    <li><a href="#" className="styled-text" data-text="Carrito">Carrito</a></li>
+                </ul>
+            </div>
+        </nav>
     
         <header>
             <h1 className='h1'>Doflamingo Joyas</h1>
