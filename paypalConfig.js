2d99dfafdef51c6b81a09f29720cbc98d13eab70
Ferdeveloperr <fernandojgom1395@gmail.com>
2024-11-04 18:21:37 -0300

    changes production

--- paypalConfig.js
+++ paypalConfig.js
@@ -1,10 +1,12 @@
 import paypal from '@paypal/checkout-server-sdk';
 
-const environment = new paypal.core.SandboxEnvironment(
-  process.env.PAYPAL_CLIENT_ID,  // Tu client ID de PayPal
-  process.env.PAYPAL_CLIENT_SECRET  // Tu client Secret de PayPal
+// Cambiar a LiveEnvironment para producción
+const environment = new paypal.core.LiveEnvironment(
+  process.env.PAYPAL_CLIENT_ID,  // Client ID de producción
+  process.env.PAYPAL_CLIENT_SECRET  // Client Secret de producción
 );
 
 const client = new paypal.core.PayPalHttpClient(environment);
 
 export default client;
+
